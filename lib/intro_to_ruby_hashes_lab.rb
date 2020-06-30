d621f1dcded043af98dac150289f60544029a90a
Claudia Borghini <claborghini@gmail.com>
2020-06-30 13:00:35 +0000

    Done.

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,16 +1,25 @@
 def new_hash
-  # return an empty hash
+  hash = {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  my_hash = {
+    "name" => "Claudia",
+    "age" => 26
+  }
 end
 
 def pioneer
+pioneer_hash = {
+  name: "Grace Hopper"
+}
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
 end
 
 
 def id_hash_generator(number)
+  id_hash = {
+    id: number,
+  }
   # return a hash with a key :id assigned to the provided number
 end
